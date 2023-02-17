559f5be7405dc7ed38b6aebe07864d95288a61fb
Nouzi <tomikitd@gmail.com>
2023-02-17 15:06:32 +0100

    Initial commit

---
+++ resources/js/Pages/index.jsx
@@ -0,0 +1,10 @@
+export default function index() {
+return (
+    <div>
+
+        <p>hello world</p>
+
+    </div>
+
+
+)}
---
+++ resources/js/Pages/spam.jsx
@@ -0,0 +1,10 @@
+export default function spam() {
+    return (
+        <div>
+<br/>
+            <p>Not hello world</p>
+
+        </div>
+
+
+    )}
